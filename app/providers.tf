33741b9ad476245d54ab75e89ccd171f5e4746cf
Anton Osmond <antonosmond@gmail.com>
2016-05-13 23:41:43 +0100

    Removed app

--- app/providers.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = "${lookup(var.region, var.environment)}"
-}
--- app/remote-states.tf
+++
@@ -1,8 +0,0 @@
-resource "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config {
-    bucket = "${var.s3_bucket}"
-    key = "${var.environment}/vpc.tfstate"
-    region = "${var.s3_region}"
-  }
-}
