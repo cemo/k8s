5141873b3cc4d1c48b44910f0c3a4f01e308cc95
Anton Osmond <antonosmond@gmail.com>
2016-05-16 00:53:02 +0100

    Updated consul role in ubuntu-base playbook to add init script

--- ubuntu-base/playbook/test/integration/default/serverspec/localhost/consul_spec.rb
+++ ubuntu-base/playbook/test/integration/default/serverspec/localhost/consul_spec.rb
@@ -5,3 +5,11 @@ describe command('consul -v') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match /0.6/ }
 end
+
+describe file('/etc/consul.d') do
+  it { should be_directory }
+end
+
+describe file('/etc/init/consul.conf') do
+  it { should exist }
+end
